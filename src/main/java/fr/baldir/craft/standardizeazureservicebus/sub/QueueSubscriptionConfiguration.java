f996efdafca3766173f0e4315200df387e1a0229
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-11-16 22:09:45 +0100

    Receive a message from classic service bus queue SDK

---
+++ src/main/java/fr/baldir/craft/standardizeazureservicebus/sub/QueueSubscriptionConfiguration.java
@@ -0,0 +1,55 @@
+package fr.baldir.craft.standardizeazureservicebus.sub;
+
+import com.microsoft.azure.servicebus.*;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
+import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
+@Configuration
+public class QueueSubscriptionConfiguration {
+
+
+    private final String azureServiceBusConnectionString;
+
+    public QueueSubscriptionConfiguration(
+            @Value("${spring.jms.servicebus.connection-string}")
+                    String azureServiceBusConnectionString) {
+        this.azureServiceBusConnectionString = azureServiceBusConnectionString;
+    }
+
+    @Bean
+    public IQueueClient queueClient() throws Exception {
+        // Connect to service bus
+        QueueClient queueClient = new QueueClient(
+                new ConnectionStringBuilder(azureServiceBusConnectionString, "sub-queue"),
+                ReceiveMode.RECEIVEANDDELETE);
+        registerReceiver(queueClient);
+
+        return queueClient;
+    }
+
+    void registerReceiver(QueueClient queueClient) throws Exception {
+        queueClient.registerMessageHandler(
+                new IMessageHandler() {
+                    public CompletableFuture<Void> onMessageAsync(IMessage message) {
+
+                        //{"this": "is a test"}
+                        System.out.printf("Message received : %s\n", new String(message.getBody()));
+
+
+                        return CompletableFuture.completedFuture(null);
+                    }
+
+                    public void notifyException(Throwable throwable, ExceptionPhase exceptionPhase) {
+                        System.out.printf(exceptionPhase + "-" + throwable.getMessage());
+                    }
+                },
+                new MessageHandlerOptions(1, true, Duration.ofMinutes(1)));
+    }
+
+}
