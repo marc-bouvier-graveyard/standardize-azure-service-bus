250bdc3981a2bd26d65c748508c79542dbd0f614
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-11-16 22:18:44 +0100

    clean up a bit

--- src/main/java/fr/baldir/craft/standardizeazureservicebus/sub/QueueSubscriptionConfiguration.java
+++ src/main/java/fr/baldir/craft/standardizeazureservicebus/sub/QueueSubscriptionConfiguration.java
@@ -3,12 +3,14 @@ package fr.baldir.craft.standardizeazureservicebus.sub;
 import com.microsoft.azure.servicebus.*;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 
 @Configuration
 public class QueueSubscriptionConfiguration {
@@ -34,22 +36,28 @@ public class QueueSubscriptionConfiguration {
     }
 
     void registerReceiver(QueueClient queueClient) throws Exception {
+
         queueClient.registerMessageHandler(
-                new IMessageHandler() {
-                    public CompletableFuture<Void> onMessageAsync(IMessage message) {
+                messageHandler(),
+                new MessageHandlerOptions(1, true, Duration.ofMinutes(1)),
+                ForkJoinPool.commonPool());
+    }
 
-                        //{"this": "is a test"}
-                        System.out.printf("Message received : %s\n", new String(message.getBody()));
+    @NotNull
+    private IMessageHandler messageHandler() {
+        return new IMessageHandler() {
+            public CompletableFuture<Void> onMessageAsync(IMessage message) {
 
+                //{"this": "is a test"}
+                System.out.printf("Message received : %s\n", new String(message.getBody()));
 
-                        return CompletableFuture.completedFuture(null);
-                    }
+                return CompletableFuture.completedFuture(null);
+            }
 
-                    public void notifyException(Throwable throwable, ExceptionPhase exceptionPhase) {
-                        System.out.printf(exceptionPhase + "-" + throwable.getMessage());
-                    }
-                },
-                new MessageHandlerOptions(1, true, Duration.ofMinutes(1)));
+            public void notifyException(Throwable throwable, ExceptionPhase exceptionPhase) {
+                System.out.printf(exceptionPhase + "-" + throwable.getMessage());
+            }
+        };
     }
 
 }
